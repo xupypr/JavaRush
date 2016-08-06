0b4033febc40dd9c333a69aff3965db7a9daa6bb
xupypr <xupypr_2003@mail.ru>
2016-08-06 22:31:33 +0300

    level 16 - Threads. sleep, join, interrupt, IsInterrupted, interrupted exception

---
+++ level16/lesson10/task02/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level16.lesson10.task02;
+
+/* Отсчет на гонках
+1. Разберись, что делает программа.
+2. Реализуй логику метода run так, чтобы каждую секунду через пробел
+выдавался отсчет начиная с countSeconds до 1, а потом слово [Марш!] (см примеры).
+3. Если нить работает 3.5 секунды или более, прерви ее методом interrupt и внутри нити выведи в консоль слово [Прервано!].
+Пример для countSeconds=4 : [4 3 2 1 Прервано!]
+4. Если нить работает менее 3.5 секунд, она должна завершиться сама.
+Пример для countSeconds=3 : [3 2 1 Марш!]
+PS: метод sleep выбрасывает InterruptedException.
+*/
+
+import sun.security.krb5.internal.TGSRep;
+
+public class Solution {
+    public static volatile int countSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run()
+        {
+            try
+            {   int sec=countSeconds;
+                while (!Thread.interrupted())
+                {
+                    if (sec==0) {System.out.println("Марш!");break;}
+                    else System.out.print(sec+ " ");
+                    sec--;
+                    Thread.sleep(1000);
+                }
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("Прервано!");
+            }
+        }
+    }
+}
---
+++ level16/lesson10/task03/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level16.lesson10.task03;
+
+/* Снова interrupt
+Создай нить TestThread.
+В методе main создай экземпляр нити, запусти, а потом прерви ее используя метод interrupt().
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        TestThread testThread=new TestThread();
+        testThread.start();
+        testThread.interrupt();
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread {
+        public void run() {
+            while (!Thread.interrupted()) {}
+        }
+    }
+}
